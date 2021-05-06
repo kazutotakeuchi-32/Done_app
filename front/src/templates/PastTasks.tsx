d423c7eb959091a293e6bb0e1f1ff985d6b3d027
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-05-06 23:43:05 +0900

    Completed implementation of PastTasks component

--- front/src/templates/PastTasks.tsx
+++ front/src/templates/PastTasks.tsx
@@ -1,15 +1,128 @@
-import React from "react"
-import { TabPanel } from "./TabPanel"
-type Props ={
-  value:number,
-  index:number
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import TablePagination from '@material-ui/core/TablePagination'
+import { ModalForm } from './modalForm'
+import { DenseTable } from './Table'
+import { TabPanel } from './TabPanel'
+
+type Props = {
+  value: number
+  index: number
 }
-export const PastTasks = (props:Props)=>{
-  const {value,index} = props
-  return(
+
+function getId():string {
+  return JSON.parse(localStorage.redux).users.id == location.href.split('/')[4]
+        ? JSON.parse(localStorage.redux).users.id
+        : location.href.split('/')[4]
+}
+
+export const PastTasks = (props: Props) => {
+  const { value, index } = props
+  const [previousTasks, setPreviousTasks] = useState<{ data: any; title: string }>({ data: [], title: '' })
+  const [open, setOpen] = useState(false)
+  const [title, setTitle] = useState('')
+  const [id, setId] = useState(0)
+  const [subject, setSubject] = useState('')
+  const [time, setTime] = useState(0)
+  const [content, setContent] = useState('')
+  const [page, setPage] = useState(0)
+  const [maxPage, setMaxPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = React.useState(10)
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const id = getId()
+      const res = await axios.get(
+        `http://localhost:3000/api/v1/learns/past_tasks?id=${id}&cuurent_page=${page + 1}&rows=${rowsPerPage}`
+      )
+      const { previousTasks, maxPage: MP } = res.data.data
+      setPreviousTasks(previousTasks)
+      setMaxPage(MP)
+    }
+    getTasks()
+  }, [])
+
+  const handleChangePage = async (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    const id = getId()
+    const res = await axios.get(
+      `http://localhost:3000/api/v1/learns/past_tasks?id=${id}&cuurent_page=${newPage + 1}&rows=${rowsPerPage}`
+    )
+    const { previousTasks, maxPage: MP } = res.data.data
+    setMaxPage(MP)
+    setPreviousTasks(previousTasks)
+    setPage(newPage)
+  }
+  const handleChangeRowsPerPage = async (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const id = getId()
+    const res = await axios.get(
+      `http://localhost:3000/api/v1/learns/past_tasks?id=${id}&cuurent_page=${1}&rows=${event.target.value}`
+    )
+    const { previousTasks, maxPage: MP } = res.data.data
+    setMaxPage(MP)
+    setPreviousTasks(previousTasks)
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+
+  return (
     <TabPanel index={index} value={value}>
-      <div className="">
-       過去のタスク
+      <div
+        className=""
+        style={{
+          maxHeight: 'auto',
+          overflow: 'scroll',
+        }}
+      >
+        {value == index && previousTasks.data.length != 0 ? (
+          <>
+            <TablePagination
+              style={{
+                background: '#333',
+                color: 'white',
+              }}
+              component="div"
+              count={maxPage * rowsPerPage}
+              page={page}
+              onChangePage={handleChangePage}
+              rowsPerPage={rowsPerPage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              rowsPerPageOptions={[10, 20, 30]}
+              labelRowsPerPage="行数"
+              labelDisplayedRows={({ from, to, count }) =>
+                `${from}-${to} / ${count !== -1 ? count + '行' : `more than ${to}行`}`
+              }
+            />
+            <DenseTable
+              draftNextTasks={previousTasks}
+              isCheckBox={false}
+              handleClick={(e) => {
+                const setTask = previousTasks.data.filter((n) => n.id == e.target.id)[0]
+                setId(setTask.id)
+                setTitle(setTask.title)
+                setSubject(setTask.subject)
+                setTime(setTask.time)
+                setContent(setTask.content)
+                setOpen(true)
+              }}
+              handleCheaked={(e) => e}
+            />
+            <ModalForm
+              open={open}
+              tasks={{ id: id, title: title, subject: subject, time: time, content: content }}
+              handleOpen={() => {
+                setOpen(true)
+              }}
+              handleClose={() => {
+                setOpen(false)
+              }}
+              handleSubmid={(vl) => vl}
+              submidText={''}
+              isReadonly={true}
+            />
+          </>
+        ) : (
+          <div className="">タスクが登録されていません。</div>
+        )}
       </div>
     </TabPanel>
   )
--- front/src/templates/TodaysTasks.tsx
+++ front/src/templates/TodaysTasks.tsx
@@ -1,17 +1,21 @@
-
-import axios from "axios"
-import React, { useEffect, useState } from "react"
-import { ModalForm } from "./modalForm"
-import { DenseTable } from "./Table"
-import { TabPanel } from "./TabPanel"
-type Props ={
-  value:number,
-  index:number
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { ModalForm } from './modalForm'
+import { DenseTable } from './Table'
+import { TabPanel } from './TabPanel'
+type Props = {
+  value: number
+  index: number
+}
+function getId():string {
+  return JSON.parse(localStorage.redux).users.id == location.href.split('/')[4]
+        ? JSON.parse(localStorage.redux).users.id
+        : location.href.split('/')[4]
 }
 
-export const TodaysTasks =(props:Props)=>{
-  const {value,index} = props
-  const [todaysTasks,setTodaysTasks] = useState<{data:any,title:string}>({data:[],title:""})
+export const TodaysTasks = (props: Props) => {
+  const { value, index } = props
+  const [todaysTasks, setTodaysTasks] = useState<{ data: any; title: string }>({ data: [], title: '' })
   const [open, setOpen] = useState(false)
   const [title, setTitle] = useState('')
   const [id, setId] = useState(0)
@@ -19,63 +23,59 @@ export const TodaysTasks =(props:Props)=>{
   const [time, setTime] = useState(0)
   const [content, setContent] = useState('')
 
-  useEffect(()=>{
-    const getTasks = async ()=>{
-      const id = JSON.parse(localStorage.redux).users.id == location.href.split("/")[4]?
-            JSON.parse(localStorage.redux).users.id
-            :
-            location.href.split("/")[4]
-     const res = await axios.get(`http://localhost:3000/api/v1/draft_learns/todays_task?id=${id}`)
-     const {nextTasks:todayTasks} = res.data.data
-     setTodaysTasks(todayTasks)
+  useEffect(() => {
+    const getTasks = async () => {
+      const id = getId()
+      const res = await axios.get(`http://localhost:3000/api/v1/learns/todays_task?id=${id}`)
+      const { nextTasks: todayTasks } = res.data.data
+      setTodaysTasks(todayTasks)
     }
-    // if (value==index) {
     getTasks()
-    // }
-  },[])
-//   <DenseTable
-//   draftNextTasks={todayTasks}
-//   handleClick={(e)=>e}
-//   handleCheaked={e=>e}
-// />
+  }, [])
 
-  return(
+  return (
     <TabPanel index={index} value={value}>
-      {
-        value==index && todaysTasks.data.length!=0  ?
-        <>
-          <DenseTable
-          draftNextTasks={todaysTasks}
-          isCheckBox={false}
-            handleClick={(e)=>{
-              const setTask = todaysTasks.data.filter((n) => n.id == e.target.id)[0]
-                      setId(setTask.id)
-                      setTitle(setTask.title)
-                      setSubject(setTask.subject)
-                      setTime(setTask.time)
-                      setContent(setTask.content)
-                      setOpen(true)
-            }}
-            handleCheaked={e=>e}
-        />
-        <ModalForm
-          open={open}
-          tasks={{ id: id, title: title, subject: subject, time: time, content: content }}
-          handleOpen={() => {
-            setOpen(true)
-          }}
-          handleClose={() => {
-            setOpen(false)
-          }}
-          handleSubmid={(vl) =>vl}
-          submidText={""}
-          isReadonly={true}
-        />
-       </>
-      :
-        <div className="">タスクが登録されていません。</div>
-      }
-
+      <div
+        className=""
+        style={{
+          maxHeight: '400px',
+          overflow: 'scroll',
+        }}
+      >
+        {value == index && todaysTasks.data.length != 0 ? (
+          <>
+            <DenseTable
+              draftNextTasks={todaysTasks}
+              isCheckBox={false}
+              handleClick={(e) => {
+                const setTask = todaysTasks.data.filter((n) => n.id == e.target.id)[0]
+                setId(setTask.id)
+                setTitle(setTask.title)
+                setSubject(setTask.subject)
+                setTime(setTask.time)
+                setContent(setTask.content)
+                setOpen(true)
+              }}
+              handleCheaked={(e) => e}
+            />
+            <ModalForm
+              open={open}
+              tasks={{ id: id, title: title, subject: subject, time: time, content: content }}
+              handleOpen={() => {
+                setOpen(true)
+              }}
+              handleClose={() => {
+                setOpen(false)
+              }}
+              handleSubmid={(vl) => vl}
+              submidText={''}
+              isReadonly={true}
+            />
+          </>
+        ) : (
+          <div className="">タスクが登録されていません。</div>
+        )}
+      </div>
     </TabPanel>
   )
 }
